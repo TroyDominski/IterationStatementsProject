ac92955c77dd112fac1e21ce7d8961da32add4d7
Troy Dominski <dominskitroy6@gmail.com>
2025-10-30 14:29:06 -0500

    IterationStatementProject

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -11,28 +11,33 @@ namespace IterationStatements
             // Use the example given to guide your implementation.
 
             //Create a List called "numbers" - DONE!
-            List<int> numbers = new List<int>();  //DO NOT ERASE THIS! You will use this in the exercise.
+            var numbers = new List<int>();  //DO NOT ERASE THIS! You will use this in the exercise.
             
             //-----START HERE------------------------------------------
-
+            
             //TODO - Create a variable of type int and name it "num"
             //TODO - Initialize the variable with a value of 0
 
+            var num = 0;
 
+            do
+                //TODO - Create a do-while loop using the existing brackets as a template.
+                // The existing brackets are placeholders for your loop code.
+                // Inside this loop:
+                // a) Increment "num" by 1
+                // b) Then add "num" to the collection "numbers"
+                //    Hint: Use "numbers.Add(num);" to add the current value of "num" to the list.
 
-            //TODO - Create a do-while loop using the existing brackets as a template.
-            // The existing brackets are placeholders for your loop code.
-            // Inside this loop:
-            // a) Increment "num" by 1
-            // b) Then add "num" to the collection "numbers"
-            //    Hint: Use "numbers.Add(num);" to add the current value of "num" to the list.
 
             {
+                num++;
                 // Your code goes here
-                numbers.Add(/*num*/); //uncomment out `num` to add it to the collection "numbers"
-            } 
-            //TODO - Continue the loop while "num" is less than 100
-            
+                numbers.Add(num); //uncomment out `num` to add it to the collection "numbers"
+
+                //TODO - Continue the loop while "num" is less than 100
+            } while (num < 100);
+
+            while (num < 200)
 
             //TODO - Create a while loop using the existing brackets as a template.
             //TODO - Continue the loop while "num" is less than 200
@@ -43,20 +48,23 @@ namespace IterationStatements
             //    Hint: You can copy how this was done in the do-while loop
 
             {
-                // Your code goes here
+                num++;
+                numbers.Add(num);
+                
             }
             
 
 
             // This is to show the user that the numbers will start increasing on the console
             Console.WriteLine("Increase:");
+            foreach(var number in numbers)
 
 
             //TODO - Create a foreach loop using the existing brackets as a template.
             // The existing brackets are placeholders for your loop code.
             // Inside this loop, print each number in "numbers".
             {
-                // Your code goes here
+                Console.WriteLine(number);
             }
              
 
@@ -70,11 +78,11 @@ namespace IterationStatements
             // This loop will print the numbers in reverse order - from 200 to 1.
 
             // a) In your initializer, set the value of "i" to 199
-            // b) In your conditional, ensure that the loop continues as long as "i" is:
+            // b In your conditional, ensure that the loop continues as long as "i" is:
             //    - Less than or equal to the number of items in "numbers" (use "numbers.Count")
             //    - Greater than or equal to 0
-            // c) Decrement "i" by 1 in each iteration
-
+            //c) Decrement "i" by 1 in each iteration
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 //TODO - Inside the loop, place numbers[i] inside of the Console.WriteLine() method
                 Console.WriteLine(numbers[i]); // Example placement of numbers[i] inside Console.WriteLine
